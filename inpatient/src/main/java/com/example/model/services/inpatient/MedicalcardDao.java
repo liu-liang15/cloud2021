05ad17870feff2120e51e0dd4780d757fccdfb62
黑黑黑 <2270716426@qq.com>
2021-09-28 16:32:27 +0800

    整合陈宇俊提交

--- inpatient/src/main/java/com/example/model/services/inpatient/MedicalcardDao.java
+++ inpatient/src/main/java/com/example/model/services/inpatient/MedicalcardDao.java
@@ -4,8 +4,6 @@ import com.pojos.inpatient.Medicalcardjfjl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author 刘亮
