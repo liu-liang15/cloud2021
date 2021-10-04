a5624e1c76aa1f4e3b53dfc9a1a219b0eedfc43c
黑黑黑 <2270716426@qq.com>
2021-10-04 15:02:30 +0800

    根据时间 科室 职位 查排班的员工

--- system/src/main/java/com/example/model/dao/system/PaiBanMapper.java
+++ system/src/main/java/com/example/model/dao/system/PaiBanMapper.java
@@ -3,6 +3,7 @@ package com.example.model.dao.system;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pojos.system.PaiBan;
+import com.pojos.system.YuanGo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -16,4 +17,5 @@ public interface PaiBanMapper extends BaseMapper<PaiBan> {
 //   查询排班表
     List<PaiBan> getPanBan(@Param("rq") String rq, @Param("ksId") String ksId);
 
+    List<YuanGo> getyg(@Param("ksId") String ksId,@Param("gwId") String gwId, @Param("rq") String rq);
 }
