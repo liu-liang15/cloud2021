a81a73a336d25c6a9cc1da1bda4bc8682b8f9edb
@beimingyouxue <leiyingjia520@foxmail.com>
2021-10-09 11:00:53 +0800

    hjm

--- pharmacy/src/main/java/com/example/controller/pharmacy/YaoFangController.java
+++ pharmacy/src/main/java/com/example/controller/pharmacy/YaoFangController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("pharmacy/")
 public class YaoFangController {
     @Autowired
     YaoFangService yaoFangService;
