385247c589b5eb39a2f319d5577f970ac05641cd
LAPTOP-498GT2SK\yujun <1687699424@qq.com>
2021-10-18 09:37:44 +0800

    yujun

--- cloud-api-commons/src/main/java/com/pojos/hyj/AssayInfo.java
+++ cloud-api-commons/src/main/java/com/pojos/hyj/AssayInfo.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -23,7 +24,7 @@ public class AssayInfo {
     private String assayPersonCard;
     private String assayPersonPhone;
     @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
-    private Timestamp assayPersonTime;
+    private Date assayPersonTime;
     private String mealName;
     private String assayMealName;
     private String ygName;
@@ -37,5 +38,20 @@ public class AssayInfo {
     private String assayFs;
     private String mealInfoNum;
     private List<AssayRes> assayRes;
-    private List<AssayMealImg> assayMealImg;
+    private List<AssayMealImg> assayMealImgs;
+
+    public AssayInfo(String assayPersonName, String assayPersonSex, int assayPersonAge, String assayPersonCard, String assayPersonPhone, Date assayPersonTime, String assayMealName, String ygName, String ksName, String keyboarder, int fedibeNo, String assayFs) {
+        this.assayPersonName = assayPersonName;
+        this.assayPersonSex = assayPersonSex;
+        this.assayPersonAge = assayPersonAge;
+        this.assayPersonCard = assayPersonCard;
+        this.assayPersonPhone = assayPersonPhone;
+        this.assayPersonTime = assayPersonTime;
+        this.assayMealName = assayMealName;
+        this.ygName = ygName;
+        this.ksName = ksName;
+        this.keyboarder = keyboarder;
+        this.fedibeNo = fedibeNo;
+        this.assayFs = assayFs;
+    }
 }
