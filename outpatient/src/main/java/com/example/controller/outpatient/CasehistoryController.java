f3c7499e9b817f602ed48fd5e3a9804e793650da
LAPTOP-498GT2SK\yujun <1687699424@qq.com>
2021-11-04 09:53:20 +0800

    11

--- outpatient/src/main/java/com/example/controller/outpatient/CasehistoryController.java
+++ outpatient/src/main/java/com/example/controller/outpatient/CasehistoryController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/outpatient")
+    @RestController
+    @RequestMapping("/outpatient")
 public class CasehistoryController {
     @Autowired
     CasehistoryService casehistoryService;
